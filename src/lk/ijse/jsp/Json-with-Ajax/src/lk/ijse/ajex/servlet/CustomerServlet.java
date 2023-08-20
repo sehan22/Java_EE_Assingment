8a721cd1ecf58b3a25127b0e4aed83674986c283
sehan22 <sehanranaweera2002@gmail.com>
2023-08-20 11:05:13 +0530

    Get all function finalize

--- src/lk/ijse/jsp/Json-with-Ajax/src/lk/ijse/ajex/servlet/CustomerServlet.java
+++ src/lk/ijse/jsp/Json-with-Ajax/src/lk/ijse/ajex/servlet/CustomerServlet.java
@@ -1,4 +1,49 @@
 package lk.ijse.ajex.servlet;
 
-public class CustomerServlet {
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.*;
+
+@WebServlet(urlPatterns = {"/pages/customer"})
+public class CustomerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/company", "root", "1234");
+            PreparedStatement pstm = connection.prepareStatement("select * from Customer");
+            ResultSet rst = pstm.executeQuery();
+
+            JsonArrayBuilder allCustomers = Json.createArrayBuilder();
+            while (rst.next()) {
+                String id = rst.getString(1);
+                String name = rst.getString(2);
+                String address = rst.getString(3);
+
+                JsonObjectBuilder customerObject = Json.createObjectBuilder();
+                customerObject.add("id", id);
+                customerObject.add("name", name);
+                customerObject.add("address", address);
+                allCustomers.add(customerObject.build());
+            }
+
+            resp.getWriter().print(allCustomers.build());
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
 }
